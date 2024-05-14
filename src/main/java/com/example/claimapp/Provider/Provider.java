7761938f65dc997a3f2601832116583251382d68
TungTran159 <s3978481@rmit.edu.vn>
2024-05-14 16:44:03 +0700

    update

--- src/main/java/com/example/claimapp/Provider/Provider.java
+++ src/main/java/com/example/claimapp/Provider/Provider.java
@@ -3,57 +3,17 @@ package com.example.claimapp.Provider;
 public class Provider {
 
     private String id;
-    private String fullName;
-    private String phone;
-    private String address;
-    private String email;
 
     public Provider() {
         this.id = "default";
-        this.fullName = "default";
-        this.phone = null;
-        this.address = null;
-        this.email = null;
     }
 
-    public Provider(String id, String fullName, String phone, String address, String email) {
+    public Provider(String id) {
         this.id = id;
-        this.fullName = fullName;
-        this.phone = phone;
-        this.address = address;
-        this.email = email;
     }
 
     public String getId() {
         return id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
 
